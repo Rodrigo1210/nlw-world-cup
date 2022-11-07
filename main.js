858da560bcc6c16f4cc5853b8db8ecb4104d897e
unknown <rodrigo.jardim@outlook.com.br>
2022-11-06 21:48:30 -0300

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+function createGame(firstTeam, hour, secondTeam) {
+    return `
+        <li>
+            <img src="./assets/icon-${firstTeam}.svg" alt="${firstTeam} flag" title="${firstTeam}">
+            <strong>${hour}</strong>
+            <img src="./assets/icon-${secondTeam}.svg" alt="${secondTeam} flag" title="${secondTeam}">
+        </li>
+    `
+}
+
+let delay = -0.3;
+function createCard(date, day, games) {
+    delay += 0.3;
+    return `
+        <div class="card" style="animation-delay: ${delay}s">
+            <h2>${date} <span>${day}</span></h2>
+
+            <ul>
+                ${games}
+            </ul>
+        </div>
+    `
+}
+
+document.querySelector('.cards').innerHTML = `
+        ${createCard("20/11", "Sunday", 
+            createGame("Qatar", "13:00", "Ecuador")
+        )}
+
+        ${createCard("21/11", "Monday",
+            createGame("Senegal", "13:00", "Netherlands") +
+            createGame("England", "10:00", "Iran") +
+            createGame("United-States", "16:00", "Wales")
+        )}
+`
